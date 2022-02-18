976f1110ab7eb269dc359123a87d7c0ae9d5d096
= <=>
2022-02-18 23:52:55 +0300

    deny access via localhost

--- public/index.php
+++ public/index.php
@@ -1,4 +1,10 @@
 <?php
+
+/**
+ * Let's utilize composer's autoload to load our classes so we don't have to worry
+ * about loading them, cool to relax right
+ */
+require_once __DIR__ .'/../vendor/autoload.php';
 /**
  * System requires PHP v7.4 or greater
  */
@@ -9,16 +15,25 @@ if (version_compare(PHP_VERSION, '7.4', 'lt'))
 
 
 /**
+ * System only runs on a virtual host in development
+ */
+if(strpos($_SERVER['HTTP_HOST'], "localhost") !== false || filter_var($_SERVER['HTTP_HOST'], FILTER_VALIDATE_IP))
+{
+	exit(
+		"
+		<h1 style='text-align: center; color: red; margin: 10px;'>Oops, " . env("APP_NAME") ." Requires a VIRTUAL HOST.</h1>
+		<h2 style='text-align: center; color: orange; margin: 10px;'>Can not be accessed on " . $_SERVER['HTTP_HOST'] ."</h2> 
+		<h3 style='text-align: center; color: green'>Please create a virtaul host for this app and access it using the 
+		vhost created!</h4>
+		"
+	);
+}
+
+/**
  * Base Project Path
  * @var BASE_PATH string
  */
 define("BASE_PATH", $_SERVER['DOCUMENT_ROOT']);
-
-/**
- * Let's utilize composer's autoload to load our classes so we don't have to worry
- * about loading them, cool to relax right
- */
-require_once '../vendor/autoload.php';
 
 use System\App\App;
 
